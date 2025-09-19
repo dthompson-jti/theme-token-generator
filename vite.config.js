767b17ff776a8602cbe6b53dbddba3b15e4d75c9
Dave <dthompson@journaltech.com>
2025-09-19 16:54:01 -0700

    working on github page settings

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/theme-token-generator/', // must match your repo name
+  base: '/theme-token-generator/', // repo name
 })
-git 
